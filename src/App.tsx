ddb92132b5d199f66a10680687274c39e3a1cd8d
Alfred Nelson <alfrednelson375@gmail.com>
2023-03-17 20:19:45 +0530

    Initial commit -> project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex text-white font-semibold text-lg justify-center items-center bg-[#755F8B]">
+      2048
+    </div>
+  );
+}
+
+export default App;
